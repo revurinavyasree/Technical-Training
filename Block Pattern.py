be4ac639d0eab572be984a6d2dde1232443b1889
Revuri Navya Sree <revurinavyasree06@gmail.com>
2025-08-19 15:24:29 +0530

    Create Block Pattern.py
    Added Third Program

---
+++ Block Pattern.py
@@ -0,0 +1,12 @@
+b=int(input("enter num of blocks"))
+l=int(input("enter num of blocks"))
+s=int(input("enter num of blocks"))
+for i  in range(b):
+    c=0
+    for j in range(l-i):
+        for k in range(s):
+            print("*",end=" ")
+            c=c+1
+        print()
+        print(c)
+    print("_________________")
