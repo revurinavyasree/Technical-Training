96becf2804095d41f0316d0eed8747f192864521
Revuri Navya Sree <revurinavyasree06@gmail.com>
2025-08-20 21:43:25 +0530

    No of words in paragraph.py

---
+++ no of words in paragraph.py
@@ -0,0 +1,10 @@
+paragraph="""Python can be used on a server to create web 
+applications.Python can be used alongside software to create 
+workflows.Python can connect to database systems. It can also 
+read and modify files.Python can be used to handle big data
+perform complex mathematics."""
+#splitting by spaces
+word=paragraph.split()
+word_count=len(word)
+
+print("no of words in the paragraph:",word_count)
